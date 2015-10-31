fdd6ce7da9f81a6d1ba9447819e4891870d3c159
Your Name <vipul.online01@gmail.com>
2015-10-31 12:43:22 +0530

    no message

--- rest.go
+++ rest.go
@@ -5,61 +5,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"strings"
-
+	"net/http"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-
-	"github.com/julienschmidt/httprouter"
+    "github.com/julienschmidt/httprouter"
 )
 
-type Person struct {
-	Name  string
-	Phone string
-}
-type MyJsonResult struct {
+
+type JsonResult struct {
+	
 	Results []struct {
-		AddressComponents []struct {
-			LongName  string   `json:"long_name"`
-			ShortName string   `json:"short_name"`
-			Types     []string `json:"types"`
-		} `json:"address_components"`
-		FormattedAddress string `json:"formatted_address"`
-		Geometry         struct {
-			Location struct {
+		
+		Geometry     struct {
+			   Location struct {
 				Lat float64 `json:"lat"`
 				Lng float64 `json:"lng"`
-			} `json:"location"`
-			LocationType string `json:"location_type"`
-			Viewport     struct {
-				Northeast struct {
-					Lat float64 `json:"lat"`
-					Lng float64 `json:"lng"`
-				} `json:"northeast"`
-				Southwest struct {
-					Lat float64 `json:"lat"`
-					Lng float64 `json:"lng"`
-				} `json:"southwest"`
-			} `json:"viewport"`
+			   } `json:"location"`
+			
 		} `json:"geometry"`
-		PlaceID string   `json:"place_id"`
-		Types   []string `json:"types"`
+		
 	} `json:"results"`
-	Status string `json:"status"`
 }
+	
 
-type MyJsonName struct {
+type JsonName struct {
 	Id         bson.ObjectId `json:"id" bson:"_id"`
 	Name       string        `json:"name"`
 	Address    string        `json:"address"`
 	City       string        `json:"city"`
 	State      string        `json:"state"`
-	Zip        string        `json:"zip"`
-	Coordinate struct {
+	Zip    string            `json:"zip"`
+	CoordinateField struct {
 		Lat float64 `json:"lat"`
 		Lng float64 `json:"lng"`
-	} `json:"coordinate"`
+	} `json:"coordinate_field"`
 }
 
 func Getlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
@@ -70,28 +50,19 @@ func Getlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params
 	}
 	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
 	c := session.DB("cmpe273").C("people")
 	id := p.ByName("name")
 	oid := bson.ObjectIdHex(id)
-	var result MyJsonName
+	var result JsonName
 	c.FindId(oid).One(&result)
-
-	if err != nil {
+    if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Searched Name:", result.Name)
-	fmt.Println("Searched Address:", result.Address)
-	fmt.Println("Searched City:", result.City)
-	fmt.Println("Searched State:", result.State)
-	fmt.Println("Searched Zip:", result.Zip)
-	fmt.Println("Searched latitude:", result.Coordinate.Lat)
-	fmt.Println("Searched longitude:", result.Coordinate.Lng)
 
-	fmt.Println("Id2:", result.Id.String())
+    fmt.Println("SeqNo:", result.Id.String())
 	oid = bson.ObjectId(result.Id)
 
 	b2, err := json.Marshal(result)
@@ -99,21 +70,21 @@ func Getlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params
 	}
 
 	fmt.Fprintf(rw, string(b2))
-	fmt.Println("Method Name: " + req.Method)
+	fmt.Println("Method : " + req.Method)
 }
 
 func Postlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
 
-	var myjson3 MyJsonName
-	s3 := json.NewDecoder(req.Body)
-	err := s3.Decode(&myjson3)
-	StartQuery := "http://maps.google.com/maps/api/geocode/json?address="
-	WhereQuery := myjson3.Address + " " + myjson3.City + " " + myjson3.State
-	WhereQuery = strings.Replace(WhereQuery, " ", "+", -1)
-	EndQuery := "&sensor=false"
-	Url1 := StartQuery + WhereQuery + EndQuery
-	fmt.Println("Published URL: " + Url1)
-	res, err := http.Get(Url1)
+	var crud JsonName
+	args := json.NewDecoder(req.Body)
+	err := args.Decode(&crud)
+	ApiCall := "http://maps.google.com/maps/api/geocode/json?address="  // Calling the API
+	LocationCall := crud.Address + " " + crud.City + " " + crud.State   // Getting the location details
+	LocationCall = strings.Replace(LocationCall, " ", "+", -1)
+	FinalCall := "&sensor=false"
+	Link := ApiCall + LocationCall + FinalCall
+	fmt.Println("Published URL: " + Link)
+	res, err := http.Get(Link)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -122,32 +93,29 @@ func Postlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Param
 	if err != nil {
 		log.Fatal(err)
 	}
-	var myjsonresult1 MyJsonResult
-	err = json.Unmarshal(robots, &myjsonresult1)
+	var jsonresult JsonResult
+	err = json.Unmarshal(robots, &jsonresult)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(myjsonresult1.Results[0].Geometry.Location.Lat)
-	fmt.Println(myjsonresult1.Results[0].Geometry.Location.Lng)
+	fmt.Println(jsonresult.Results[0].Geometry.Location.Lat)
+	fmt.Println(jsonresult.Results[0].Geometry.Location.Lng)
 
-	myjson3.Id = bson.NewObjectId()
-	fmt.Println("Check1")
-	fmt.Println(string(myjson3.Id))
-	fmt.Println(myjson3.Id.Hex())
-	fmt.Println(myjson3.Id.String())
-	fmt.Println(myjson3.Id.Pid())
+	crud.Id = bson.NewObjectId()
+	
+    crud.CoordinateField.Lat = jsonresult.Results[0].Geometry.Location.Lat
+	crud.CoordinateField.Lng = jsonresult.Results[0].Geometry.Location.Lng
 
-	myjson3.Coordinate.Lat = myjsonresult1.Results[0].Geometry.Location.Lat
-	myjson3.Coordinate.Lng = myjsonresult1.Results[0].Geometry.Location.Lng
-
-	fmt.Println("Name " + myjson3.Name)
-	fmt.Println("\nAddress: " + myjson3.Address)
-	fmt.Println("\nCity:  " + myjson3.City)
-	fmt.Println("\nState: " + myjson3.State)
+	fmt.Println("Name " + crud.Name)
+	fmt.Println("\nAddress: " + crud.Address)
+	fmt.Println("\nCity:  " + crud.City)
+	fmt.Println("\nState: " + crud.State)
 	fmt.Println("\nLat and long : ")
 
-	fmt.Println(myjson3.Coordinate.Lat)
-	fmt.Println(myjson3.Coordinate.Lng)
+	fmt.Println(crud.CoordinateField.Lat)
+	fmt.Println(crud.CoordinateField.Lng)
+	fmt.Println("\n Zip: " + crud.Zip)
+		
 
 	if err != nil {
 	}
@@ -158,27 +126,25 @@ func Postlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Param
 	}
 	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 	c := session.DB("cmpe273").C("people")
 
-	err = c.Insert(myjson3)
+	err = c.Insert(crud)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	result := MyJsonName{}
-	fmt.Println()
-	id := myjson3.Id.Hex()
+	result := JsonName{}
+	id := crud.Id.Hex()
 	oid := bson.ObjectIdHex(id)
 	c.FindId(oid).One(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("New Name:", result.Name)
+	fmt.Println("Updated Name:", result.Name)
 	fmt.Println("Address:", result.Address)
-	fmt.Println("Id2:", result.Id.String())
+	fmt.Println("SeqNo:", result.Id.String())
 	oid = bson.ObjectId(result.Id)
 
 	b2, err := json.Marshal(result)
@@ -189,9 +155,9 @@ func Postlocations(rw http.ResponseWriter, req *http.Request, p httprouter.Param
 }
 
 func PutLocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
-	var myjson3 MyJsonName
-	s3 := json.NewDecoder(req.Body)
-	err := s3.Decode(&myjson3)
+	var crud JsonName
+	args := json.NewDecoder(req.Body)
+	err := args.Decode(&crud)
 
 	session, err := mgo.Dial("mongodb://Vipul01:1234@ds029804.mongolab.com:29804/cmpe273")
 	if err != nil {
@@ -199,48 +165,50 @@ func PutLocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params
 	}
 	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
 	c := session.DB("cmpe273").C("people")
 
 	id := p.ByName("name")
 	oid := bson.ObjectIdHex(id)
-	var result MyJsonName
+	var result JsonName
 	c.FindId(oid).One(&result)
-	fmt.Println("Old Name:", result.Name)
-	fmt.Println("Old Address:", result.Address)
-	fmt.Println("Old City:", result.City)
-	fmt.Println("Old State:", result.State)
-	fmt.Println("Old Zip:", result.Zip)
-	fmt.Println("Old latitude:", result.Coordinate.Lat)
-	fmt.Println("Old longitude:", result.Coordinate.Lng)
+	
+// Printing all the values......
 
-	if myjson3.Name != "" {
-		result.Name = myjson3.Name
+	fmt.Println("Name:", result.Name)
+	fmt.Println("Address:", result.Address)
+	fmt.Println("City:", result.City)
+	fmt.Println("State:", result.State)
+	fmt.Println("Zip:", result.Zip)
+	fmt.Println("latitude:", result.CoordinateField.Lat)
+	fmt.Println("longitude:", result.CoordinateField.Lng)
+
+	if crud.Name != "" {
+		result.Name = crud.Name
 	}
-	if myjson3.Address != "" {
-		result.Address = myjson3.Address
+	if crud.Address != "" {
+		result.Address = crud.Address
 	}
-	if myjson3.City != "" {
-		result.City = myjson3.City
+	if crud.City != "" {
+		result.City = crud.City
 	}
-	if myjson3.State != "" {
-		result.State = myjson3.State
+	if crud.State != "" {
+		result.State = crud.State
 	}
-	if myjson3.Zip != "" {
-		result.Zip = myjson3.Zip
+	if crud.Zip != "" {
+		result.Zip = crud.Zip
 	}
 
-	StartQuery := "http://maps.google.com/maps/api/geocode/json?address="
-	WhereQuery := result.Address + " " + result.City + " " + result.State
-	WhereQuery = strings.Replace(WhereQuery, " ", "+", -1)
+	ApiCall := "http://maps.google.com/maps/api/geocode/json?address="
+	LocationCall := result.Address + " " + result.City + " " + result.State
+	LocationCall = strings.Replace(LocationCall, " ", "+", -1)
 
-	EndQuery := "&sensor=false"
-	Url1 := StartQuery + WhereQuery + EndQuery
-	fmt.Println("Published URL: " + Url1)
+	FinalCall := "&sensor=false"
+	Link := ApiCall + LocationCall + FinalCall
+	fmt.Println("Published URL: " + Link)
 
-	res, err := http.Get(Url1)
+	res, err := http.Get(Link)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -249,18 +217,18 @@ func PutLocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params
 	if err != nil {
 		log.Fatal(err)
 	}
-	var myjsonresult1 MyJsonResult
-	err = json.Unmarshal(robots, &myjsonresult1)
+	var jsonresult JsonResult
+	err = json.Unmarshal(robots, &jsonresult)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("New latitude longiteude")
 
-	fmt.Println(myjsonresult1.Results[0].Geometry.Location.Lat)
-	fmt.Println(myjsonresult1.Results[0].Geometry.Location.Lng)
+	fmt.Println(jsonresult.Results[0].Geometry.Location.Lat)
+	fmt.Println(jsonresult.Results[0].Geometry.Location.Lng)
 
-	result.Coordinate.Lat = myjsonresult1.Results[0].Geometry.Location.Lat
-	result.Coordinate.Lng = myjsonresult1.Results[0].Geometry.Location.Lng
+	result.CoordinateField.Lat = jsonresult.Results[0].Geometry.Location.Lat
+	result.CoordinateField.Lng = jsonresult.Results[0].Geometry.Location.Lng
 
 	c.UpdateId(oid, result)
 	if err != nil {
@@ -268,14 +236,14 @@ func PutLocations(rw http.ResponseWriter, req *http.Request, p httprouter.Params
 	}
 
 	fmt.Println("\n\nNew Name:", result.Name)
-	fmt.Println("New Address:", result.Address)
-	fmt.Println("New City:", result.City)
-	fmt.Println("New State:", result.State)
-	fmt.Println("New Zip:", result.Zip)
-	fmt.Println("New latitude:", result.Coordinate.Lat)
-	fmt.Println("New longitude:", result.Coordinate.Lng)
+	fmt.Println("Updated Address:", result.Address)
+	fmt.Println("Updated City:", result.City)
+	fmt.Println("Updated State:", result.State)
+	fmt.Println("Updated Zip:", result.Zip)
+	fmt.Println("Updated latitude:", result.CoordinateField.Lat)
+	fmt.Println("Updated longitude:", result.CoordinateField.Lng)
 
-	fmt.Println("Id2:", result.Id.String())
+	fmt.Println("SeqNo:", result.Id.String())
 	oid = bson.ObjectId(result.Id)
 
 	b2, err := json.Marshal(result)
@@ -296,7 +264,6 @@ func DeleteLocations(rw http.ResponseWriter, req *http.Request, p httprouter.Par
 	}
 	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
 	c := session.DB("cmpe273").C("people")
@@ -305,6 +272,7 @@ func DeleteLocations(rw http.ResponseWriter, req *http.Request, p httprouter.Par
 	c.RemoveId(oid)
 	fmt.Fprintf(rw, "Deleted, %s!\n", p.ByName("name"))
 }
+
 
 func main() {
 
